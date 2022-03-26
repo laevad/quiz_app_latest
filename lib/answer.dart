c4b6ccfaf7946d614ebdf71ca9cbccf75a538343
laevad <djaldave@gmail.com>
2022-03-27 03:08:39 +0800

    mapping text to widget

--- lib/answer.dart
+++ lib/answer.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 class Answer extends StatelessWidget {
-  final Function answer;
-  const Answer({Key? key, required this.answer}) : super(key: key);
+  final Function answerHandler;
+  final String answerText;
+  const Answer(
+      {Key? key, required this.answerHandler, required this.answerText})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 50),
       child: ElevatedButton(
-        onPressed: () => answer(),
-        child: Text('Answer'),
+        onPressed: () => answerHandler(),
+        child: Text(answerText),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/answer.dart';
 import 'package:quiz_app/question.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   State<MyApp> createState() => _MyAppState();
 }
@@ -16,23 +18,38 @@ class _MyAppState extends State<MyApp> {
     setState(() {
       _questionIndex++;
     });
-    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
-    var question = ["fav color?", "fav fruit"];
+    var question = [
+      {
+        'question': "fav color?",
+        "answer": ['red', 'blue', 'violet']
+      },
+      {
+        "question": "fav fruit",
+        "answer": ["mango", "strawberry"]
+      },
+    ];
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'My App',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('My App'),
+          title: const Text('My App'),
         ),
         body: Column(
           children: [
-            Question(text: question[_questionIndex]),
-            Answer(answer: answerQuestion),
+            Question(text: question[_questionIndex]["question"].toString()),
+            ...(question[_questionIndex]["answer"] as List<String>)
+                .map(
+                  (answer) => Answer(
+                    answerHandler: answerQuestion,
+                    answerText: answer,
+                  ),
+                )
+                .toList(),
           ],
         ),
       ),
