c8d756712a58cb18f597ffd2e86aefa4bbb28d6d
laevad <djaldave@gmail.com>
2022-03-26 19:02:58 +0800

    simple stater quiz

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
+import 'package:flutter/material.dart';
 
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: Column(
+          children: [
+            Text('Question'),
+            ElevatedButton(
+              onPressed: null,
+              child: Text('Answer 0'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
